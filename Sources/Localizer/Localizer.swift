7977d3fae07a446bb4a0ea138dd46e280a765a92
Nicolas Degen <ndegen@me.com>
2021-01-25 20:53:45 +0100

    make it work

--- Sources/Localizer/Localizer.swift
+++ Sources/Localizer/Localizer.swift
@@ -5,12 +5,23 @@ class BundleIdentifier {}
 
 public extension Bundle {
   static var localizer: Bundle {
-    Bundle(for: BundleIdentifier.self)
+    Bundle.module
   }
 }
 
-struct Test: View {
-  var body: some View {
-    Text("key", tableName: "Table", bundle: .localizer, comment: "Test")
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, macCatalyst 13, *)
+public extension Text {
+  init(localized key: LocalizedStringKey) {
+    self.init(key, bundle: .localizer)
+  }
+}
+
+public extension String {
+  func localized() -> String {
+    NSLocalizedString(self, bundle: .localizer, comment: self)
+  }
+  
+  func localize(forBundle bundle: Bundle = .localizer, table: String? = nil, comment: String? = nil) -> String {
+    NSLocalizedString(self, tableName: table, bundle: bundle, comment: comment ?? self)
   }
 }
--- Tests/LocalizerTests/LocalizerTests.swift
+++ Tests/LocalizerTests/LocalizerTests.swift
@@ -6,7 +6,7 @@ final class LocalizerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Localizer().text, "Hello, World!")
+      XCTAssertEqual("test", "test")
     }
 
     static var allTests = [
